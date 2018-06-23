d0e66534b5e525a974549c69e68a81d12316dc9e
Andreas Nordal <andreas_nordal_4@hotmail.com>
2018-06-23 21:09:55 +0200

    Command position: Improve test

--- moduletests/expected/control_structures.bash
+++ moduletests/expected/control_structures.bash
@@ -1,18 +1,96 @@
-if [[ -v $julenissen ]]; then
-	[[ $julenissen ]] || [[ $kake ]] || exit 1
-	env [[ He is a "$n"åså ]]
-	a a a a | a a a a ; a a a a & a a a a
+let ivar be the test pilot
+
+#
+# Ivar is in command position.
+#
+[[ $ivar ]] && [[ $ivar ]] || [[ $ivar ]]; [[ $ivar ]] & [[ $ivar ]] | [[ $ivar ]]
+
+if [[ $ivar ]]; then [[ $ivar ]]; elif [[ $ivar ]]; then [[ $ivar ]]; else [[ $ivar ]]; fi
+if [[ $ivar ]]
+then
+	[[ $ivar ]]
+elif [[ $ivar ]]
+then
+	[[ $ivar ]]
+else
+	[[ $ivar ]]
 fi
 
-# Comment above command
-true
+while [[ $ivar ]]; do [[ $ivar ]]; done
+while [[ $ivar ]]
+do
+	[[ $ivar ]]
+done
 
-if true; then true true; fi
+until [[ $ivar ]]; do [[ $ivar ]]; done
+until [[ $ivar ]]
+do
+	[[ $ivar ]]
+done
 
-echo It is not possible \
-# FIXME \
-echo to comment between args
+for i in {,}; do [[ $i ]]; done
+for i in {,}
+do
+	[[ $i ]]
+done
 
-if test -e julenissen; then echo ja; elif test -e kake; then echo ok; else echo nei; fi
-for i in "$a"; do echo "$i"; done
-case "$PATH" in *"$HOME"*) echo homeopath;; esac
+true \
+# Comments don't have line continuations. \
+[[ $ivar ]]
+
+"$([[ $ivar ]])"
+<([[ $ivar ]])
+>([[ $ivar ]])
+([[ $ivar ]])
+{ [[ $ivar ]] } [[ $ivar ]]
+f() { [[ $ivar ]] }
+f()
+{
+	[[ $ivar ]]
+}
+
+oddvar="$(
+	case true in
+		# comment
+		true) # comment
+			# comment
+			[[ $ivar ]] # comment
+			# comment
+		;; # comment
+		# comment
+	esac
+)here is where the string continues"
+
+#
+# Ivar is an argument.
+#
+true [[ "$ivar" ]] && true [[ "$ivar" ]] || true [[ "$ivar" ]]; true [[ "$ivar" ]] & true [[ "$ivar" ]] | true [[ "$ivar" ]]
+[ "$ivar" ] && [ "$ivar" ] || [ "$ivar" ]; [ "$ivar" ] & [ "$ivar" ] | [ "$ivar" ]
+test "$ivar" && test "$ivar" || test "$ivar"; test "$ivar" & test "$ivar" | test "$ivar"
+
+if true [[ "$ivar" ]]; then true [[ "$ivar" ]]; elif true [[ "$ivar" ]]; then true [[ "$ivar" ]]; else true [[ "$ivar" ]]; fi
+if true [[ "$ivar" ]]
+then
+	true [[ "$ivar" ]]
+elif true [[ "$ivar" ]]
+then
+	true [[ "$ivar" ]]
+else
+	true [[ "$ivar" ]]
+fi
+
+echo line continuation \
+[[ "$ivar" ]]
+
+{[[ "$ivar" ]]}
+echo {} [[ "$ivar" ]]
+
+for i in [[ "$ivar" ]]; do :; done
+select i in [[ "$ivar" ]]; do break; done
+
+array=(
+	[[ "$ivar" ]]
+)
+array+=(
+	[[ "$ivar" ]]
+)
