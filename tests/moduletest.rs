75aa886ea29bea873e89131971812d95178b47d9
Andreas Nordal <andreas_nordal_4@hotmail.com>
2020-09-20 11:38:28 +0200

    Moduletest: Test only the executable for the right build
    Requires Cargo 1.43.0 (for CARGO_BIN_EXE_<name>):
    https://github.com/rust-lang/rust/blob/1.43.0/RELEASES.md
    
    Fixes #35

--- tests/moduletest.rs
+++ tests/moduletest.rs
@@ -1,23 +1,11 @@
+use std::env;
 use std::process::Command;
-
-// Find the executable (https://github.com/rust-lang/cargo/issues/3670)
-const BASH_RUN_MODULETEST : &str = "
-	shopt -s failglob globstar\n\
-	if ! [[ -v CARGO_TARGET_DIR ]]; then\n\
-		CARGO_TARGET_DIR=.\n\
-	fi\n\
-	for exe in \"$CARGO_TARGET_DIR\"/**/shellharden; do\n\
-		if test -f \"$exe\" && test -x \"$exe\"; then\n\
-			moduletests/run \"$exe\" moduletests/original/*\n\
-		fi\n\
-	done\n\
-";
 
 #[test]
 fn moduletest() {
-	let mut child = Command::new("bash")
-		.arg("-ec")
-		.arg(BASH_RUN_MODULETEST)
+	let mut child = Command::new("moduletests/run")
+		.arg(env!("CARGO_BIN_EXE_shellharden"))
+		.arg("moduletests")
 		.spawn()
 		.expect("moduletests/run: Command not found")
 	;
